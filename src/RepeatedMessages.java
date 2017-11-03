283e5868747a93e1a82777d3659013ceaec65f3a
doylecm <32971939+doylecm@users.noreply.github.com>
2017-11-02 22:00:42 -0500

    Correction

--- src/RepeatedMessages.java
+++ src/RepeatedMessages.java
@@ -8,7 +8,7 @@ public class RepeatedMessages {
 	public static void main(String[] args) {
 		for (int i = 1; i <= 10; i++) {
 			for (int j = 1; j <= 100; j++) {
-				System.out.println("Milhouse tested cootie negative");
+				System.out.println("Milhouse tested cootie positive");
 			}
 		}
 	}
